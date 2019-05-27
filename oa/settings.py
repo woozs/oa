dc045648b0dd0da7511da15cd5c7f5da7f926ab6
woozs <mrwuzs@outlook.com>
2019-05-27 21:33:02 +0800

    day14-django练习

--- oa/settings.py
+++ oa/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'oa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oa',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
